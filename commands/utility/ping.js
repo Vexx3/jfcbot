6d2b5f4490dab9ba51b7bfc547fc492171e5e847
Vexx3 <zekeloo2008@gmail.com>
2024-11-08 10:50:29 +0800

    Project Initalized

---
+++ commands/utility/ping.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  cooldown: 5,
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Check the bot's latency and connection speed."),
+  async execute(interaction) {
+    const websocketPing = interaction.client.ws.ping;
+
+    const sent = await interaction.reply({
+      content: "🏓 Pinging...",
+      fetchReply: true,
+    });
+
+    const roundtripLatency =
+      sent.createdTimestamp - interaction.createdTimestamp;
+
+    await interaction.editReply(
+      `🏓 Pong! Here are my current stats:\n\n` +
+        `**🌐 WebSocket Heartbeat:** \`${websocketPing}ms\`\n` +
+        `**⏱️ Roundtrip Latency:** \`${roundtripLatency}ms`
+    );
+  },
+};
